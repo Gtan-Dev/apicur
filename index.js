24dd797256b31301b7b3b6c610a9ca710824bc56
Gtan-Dev <jmvsheke100@gmail.com>
2025-03-10 13:44:33 +0200

    index and home pages modified

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const calculateAvg = (n1, n2, n3) => (n1 + n2 + n3) / 3
-let average = calculateAvg(10,16,6)
-console.log(average);
+const greet = (name) => {
+    document.write(`Hello, ${name}`)
+}
